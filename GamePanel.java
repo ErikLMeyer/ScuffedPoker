a1c32240e5b729380ecce26b9f6adcc386463517
ErikLMeyer <erikmeyer@lewisu.edu>
2021-04-28 18:17:40 -0500

    Paint changes
    Paints hand. Some variables changed.

--- GamePanel.java
+++ GamePanel.java
@@ -3,15 +3,17 @@ import javax.swing.*;
 import java.util.ArrayList;
 
 public class GamePanel extends JPanel{
+    private PokerGame poker;
+    private Game game;
     private ArrayList<Integer> toDiscard;
-    private int handWidth, handSpacing, handX, handY;
+    private int handWidth, spacing, handX, handY;
     private int deckX, deckY, deckWidth;
 
     public ArrayList<Integer> getList(){ return toDiscard; }
 
     public int gethWidth(){ return handWidth; }
 
-    public int getSpacing(){ return handSpacing; }
+    public int getSpacing(){ return spacing; }
 
     public int getHandX(){ return handX; }
 
@@ -23,14 +25,21 @@ public class GamePanel extends JPanel{
 
     public int getDeckWidth(){ return deckWidth; }
 
-    public void setCardWidth(int w){
-        deckWidth = w;
-        handWidth = w /* * number of cards in hand */;
+    public void setCardWidthAndPosition(int w, int h){
+        deckWidth = w / 12;
+        deckX = w / 3;
+        deckY = h / 10;
+        spacing = deckWidth;
+
+        handX = deckX - (3 * deckWidth) + (2 * deckWidth / 5);
+        handY = deckY + game.getHeight() + spacing;
     }
 
-    public void setSpacing(int s){ handSpacing = s; }
+    public void setSpacing(int s){ spacing = s; }
 
     public GamePanel(){
+        poker = new PokerGame(200);
+        game = poker.getGame();
         toDiscard = new ArrayList<Integer>();
     }
 
@@ -39,16 +48,26 @@ public class GamePanel extends JPanel{
 
         int panelWidth = getWidth();
         int panelHeight = getHeight();
+        setCardWidthAndPosition(panelWidth, panelHeight);
 
         g.setColor(Color.GREEN);
         g.fillRect(0, 0, panelWidth, panelHeight);
 
-        /* 
-            set deck position and dimensions
-            paintDeck(g);
+        game.setDimension(deckWidth);
+        game.setX(deckX);
+        game.setY(deckY);
+        game.paintDeck(g);
 
-            set hand dimensions and position
+        /*
+            draw some information to the side of the deck
             for(size of hand) {paintCardStandard(g);}
         */
+       
+        for (int i = 0; i < game.getHand().size(); i++){
+            game.getHand().get(i).setWidth(deckWidth);
+            game.getHand().get(i).setY(handY);
+            game.getHand().get(i).setX(handX + (deckWidth * i) + (spacing * i));
+            game.getHand().get(i).paintCardStandard(g);
+        }
     }
 }
