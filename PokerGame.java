2618151c578b4b8e08e6bb9512c325cd03ba6dc8
ErikLMeyer <erikmeyer@lewisu.edu>
2021-04-28 18:16:25 -0500

    Expanded body

--- PokerGame.java
+++ PokerGame.java
@@ -1,10 +1,32 @@
 public class PokerGame {
-    private final int STARTING_POINTS = 200;
     private Game thisGame;
+    private int bank;
+    private int bet;
 
-    PokerGame(){
+    public Game getGame(){ return thisGame; }
+
+    public int getBank(){ return bank; }
+
+    public int getBet(){ return bet; }
+
+    public void setBank(int b){ bank = b; }
+
+    public void setBet(int b){ bet = b; }
+
+    public void runGame(){
+        thisGame.shuffle();
+        // prompt user to place a bet
+        thisGame.deal(5);
+        // wait for user to select cards to discard
+        // calculate winnings
+
+    }
+
+    PokerGame(int b){
         thisGame = new Game();
+        bank = b;
         thisGame.customDeck(13, 4);
         thisGame.shuffle();
+        thisGame.deal(5);
     }
 }
