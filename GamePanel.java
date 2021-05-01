b5edadc8b1e4f499b365ffeb227d8be48bf6fd06
ErikLMeyer <erikmeyer@lewisu.edu>
2021-05-01 11:32:33 -0500

    Implemented interface
    Implemented mouse listener. Things now do stuff when clicked on.

--- GamePanel.java
+++ GamePanel.java
@@ -1,15 +1,16 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
+
 import java.util.ArrayList;
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements MouseListener{
     private PokerGame poker;
     private Game game;
-    private ArrayList<Integer> toDiscard;
+    private ArrayList<Boolean> drawBorder;
     private int handWidth, spacing, handX, handY;
+    private int cardsInHand, cardsSelected;
     private int deckX, deckY, deckWidth;
-
-    public ArrayList<Integer> getList(){ return toDiscard; }
 
     public int gethWidth(){ return handWidth; }
 
@@ -40,7 +41,29 @@ public class GamePanel extends JPanel{
     public GamePanel(){
         poker = new PokerGame(200);
         game = poker.getGame();
-        toDiscard = new ArrayList<Integer>();
+        cardsInHand = game.getHand().size();
+        drawBorder = new ArrayList<Boolean>();
+        for (int i = 0; i < cardsInHand; i++){
+            drawBorder.add(false);
+        }
+
+        addMouseListener(this);
+    }
+
+    public void panelDiscard(){
+        int toDiscard[] = new int[cardsSelected];
+        int counter = 0;
+        System.out.print("From panel: to discard:");
+        for( int i = 0; i < cardsInHand; i++){
+            if (drawBorder.get(i)){
+                toDiscard[counter] = i;
+                System.out.print(" " + toDiscard[counter]);
+                counter++;
+                drawBorder.set(i, false);
+            }
+        }
+        game.discard(toDiscard);
+        cardsSelected = 0;
     }
 
     public void paintComponent(Graphics g){
@@ -59,15 +82,80 @@ public class GamePanel extends JPanel{
         game.paintDeck(g);
 
         /*
-            draw some information to the side of the deck
-            for(size of hand) {paintCardStandard(g);}
+            draw some information to the side of the deck such as:
+                bank
+                current bet
+            perhaps:
+                credit
+                how many hands to pay back credit
         */
        
         for (int i = 0; i < game.getHand().size(); i++){
+            int cardX = handX + (deckWidth * i) + (spacing * i);
             game.getHand().get(i).setWidth(deckWidth);
             game.getHand().get(i).setY(handY);
-            game.getHand().get(i).setX(handX + (deckWidth * i) + (spacing * i));
+            game.getHand().get(i).setX(cardX);
             game.getHand().get(i).paintCardStandard(g);
+            if (drawBorder.get(i)){
+                g.setColor(Color.RED);
+                g.drawRect(cardX - 1, handY - 1, deckWidth + 2, (int)(deckWidth * 1.4) + 2);
+            }
         }
+
+        // Draw string with various instructions
+    }
+
+    public void mouseClicked(MouseEvent e){
+        if (e.getX() >= deckX && e.getX() <= deckX + deckWidth){
+            if (e.getY() >= deckY && e.getY() <= deckY + (deckY * 1.4)){
+                panelDiscard();
+                repaint();
+            }
+        }
+        if (e.getX() >= handX && 
+            e.getX() <= handX + (deckWidth * cardsInHand) + (spacing * cardsInHand)){
+                if (e.getY() >= handY && e.getY() <= handY + (handY * 1.4)){
+                    int selectedCard = (e.getX() - handX) / (deckWidth + spacing);
+                    if (drawBorder.get(selectedCard)){
+                        drawBorder.set(selectedCard, false);
+                        cardsSelected--;
+                    } else{
+                        drawBorder.set(selectedCard, true);
+                        cardsSelected++;
+                    }
+                    System.out.println("Number of cards selected: " + cardsSelected);
+                    System.out.print("Cards selected:");
+                    for (int i = 0; i < cardsInHand; i++){
+                        if(drawBorder.get(i)){
+                            System.out.print(" " + i);
+                        }
+                    }
+                    repaint();
+                }
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
     }
 }
