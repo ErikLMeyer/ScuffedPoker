26ac30358f5ab6a2022f47fbaf6c2dec42a53e82
ErikLMeyer <erikmeyer@lewisu.edu>
2021-05-04 22:07:11 -0500

    Consolidated.

--- ScuffedPoker.java
+++ ScuffedPoker.java
@@ -1,37 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
 
-class CardTestPanel extends JPanel{
-    private Card c;
-
-    public void setCard(Card ca){ c = ca; }
-
-    public Card getCard() { return c; }
-
-    CardTestPanel(){
-        c = new Card();
-    }
-
-    CardTestPanel(Card ca){
-        c = ca;
-    }
-
-    public void paintComponent(Graphics g){
-        super.paintComponent(g);
-
-        int panelWidth = getWidth();
-        int panelHeight = getHeight();
-        c.setWidth(panelWidth / 4);
-        c.setX(50);
-        c.setY(50);
-
-        g.setColor(Color.GREEN);
-        g.fillRect(0, 0, panelWidth, panelHeight);
-
-        c.paintCardStandard(g);
-    }
-}
-
 public class ScuffedPoker{
 
     public static void printHand(Game g){
@@ -40,43 +9,54 @@ public class ScuffedPoker{
             System.out.print(g.getHand().get(i) + "; ");
         }
     }
+
+    public static void getBet(PokerFrame p){
+        boolean invalidInput = false;
+        String input;
+        int bank = p.getPanel().getGame().getBank();
+        int bet = 0;
+        String boxMessage = "Current funds: " + bank + "\nPlace your bet:";
+        do{
+            invalidInput = false;
+            try{
+                input = JOptionPane.showInputDialog(p, boxMessage);
+                bet = Integer.parseInt(input);
+            } catch(NumberFormatException e){
+                JOptionPane.showMessageDialog(p, "Error: bet must be a number.", "Invalid bet", 
+                                                JOptionPane.PLAIN_MESSAGE);
+                invalidInput = true;
+                continue;
+            }
+            if (bet > bank){
+                JOptionPane.showMessageDialog(p, "You cannot bet more than you have.", 
+                                                "Invalid bet", JOptionPane.PLAIN_MESSAGE);
+                invalidInput = true;
+            }
+        } while(invalidInput);
+        p.getPanel().getGame().setBet(bet);
+        p.getPanel().getGame().setBank(bank - bet);
+    }
+
     public static void main(String args[]){
         System.out.println("Coming soon: Poker!");
-
-        Game testGame = new Game();
-        System.out.println("Creating new deck with 4 suites of 13 numbers.");
-        testGame.customDeck(13, 4);
-        testGame.shuffle();
-        System.out.println(testGame);
-        System.out.println("Dealing 5 cards from deck.");
-        testGame.deal(5);
-        System.out.println(testGame);
-
-        System.out.println("Printing cards in hand and deck.");
-        printHand(testGame);
-
-        /* System.out.print("\nDeck: ");
-        for (int i = 0; i < testGame.getDeck().size(); i++){
-            System.out.print(testGame.getDeck().get(i) + "; ");
-            if ((i + 1) % 5 == 0)
-                System.out.print("\n");
-        } */
-
-        System.out.println("\nDiscarding 2 cards.");
-        int throwOut[] = {1,4};
-        testGame.discard(throwOut);
-        printHand(testGame);
-
-        System.out.println("\nPainting the game board.");
         PokerFrame testCardFrame = new PokerFrame();
-        /* JFrame testCardFrame = new JFrame();
-        testCardFrame.setTitle("Card");
-        testCardFrame.setBounds(100, 100, 250, 350);
-        testCardFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        Container cardPane = testCardFrame.getContentPane();
-        cardPane.add(new CardTestPanel(testGame.getHand().get(2))); */
-
+        int handCounter = 0;
+        boolean inDebt = false;
         testCardFrame.setVisible(true);
+        while(true){
+            System.out.println("Get bet");
+            getBet(testCardFrame);
+            System.out.println("Deal cards");
+            testCardFrame.getPanel().resetGame();
+            System.out.println("Wait for user to select cards.");
+            while(!testCardFrame.getPanel().getAccepted()){
+                // This doesn't work unless I have a print for some reason.
+                System.out.print("");
+                continue;
+            }
+            System.out.println("User has selected.");
+            testCardFrame.getPanel().setAccepted(false);
+            // calculate winnings or subtract loser's bet from bank
+        }
     }
 }
